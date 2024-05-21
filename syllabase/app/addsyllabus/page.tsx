81329d30ebb796581def96fef8f26d66ef740b81
Divyesh Thirukonda Gowri Shankar <thiru063@umn.edu>
2024-05-21 13:16:49 -0500

    UI fixes

--- syllabase/app/addsyllabus/page.tsx
+++ syllabase/app/addsyllabus/page.tsx
@@ -19,10 +19,11 @@ export default function Home() {
       <div style={{ marginTop: '15vh', display: 'flex', justifyContent: 'top', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
         <h1 style={{fontSize: '4.5vw'}}>Add Syllabus</h1>
 
-        <p style={{fontSize: '1vw', marginTop: '5vh'}}>Help other students get important info before they sign up for a class! Our website heavily relies on student input to get the latest data.</p>
-        <p style={{fontSize: '1vw', marginTop: '5vh', marginBottom: '15vh'}}>Open to Professors and Students.</p>
+        <p style={{fontSize: '1vw', marginTop: '5vh'}}>Help other students get important info before they sign up for a class!</p>
+        <p style={{fontSize: '1vw', marginTop: '5vh'}}>Our website heavily relies on student input to get the latest data.</p>
+        <p style={{fontSize: '1vw', marginTop: '5vh', marginBottom: '10vh'}}>Open to Professors and Students.</p>
 
-        <button style={{backgroundColor: '#2289e6', color: 'white', cursor: 'pointer', borderRadius: '15px', padding: '8px 16px', fontSize: '1vw', width: '15vw', height: '7.5vh'}}
+        <button style={{backgroundColor: '#2289e6', color: 'white', cursor: 'pointer', borderRadius: '15px', padding: '8px 16px', fontSize: '1.5vw', width: '15vw', height: '7.5vh', fontWeight: 'bold'}}
                 onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLScsgDyD8AvbeKbkcbmWP0WFGIJF580QSsgO18gn6P7mq3SfWA/viewform', '_blank')}
                 >
             CLICK HERE
--- syllabase/app/page.tsx
+++ syllabase/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <p style={{fontSize: '2vw', marginTop: '5vh', marginBottom: '20vh'}}>It's about time we know what we're signing up for</p>
 
         <div style={{ display: 'flex', alignItems: 'center'}}>
-          <button style={{backgroundColor: '#2289e6', color: 'white', cursor: 'pointer', borderRadius: '15px', marginRight: '5vw', padding: '8px 16px', fontSize: '2vw', width: '20vw', fontWeight: 'bold'}}
+          <button style={{backgroundColor: '#2289e6', color: 'white', cursor: 'pointer', borderRadius: '15px', marginRight: '5vw', padding: '8px 16px', fontSize: '2vw', width: '20vw', height: '10vh', fontWeight: 'bold'}}
                   onClick={() => router.push('/addsyllabus')}>
             ADD SYLLABUS
           </button>
@@ -32,7 +32,7 @@ export default function Home() {
               value={query}
               onChange={(e) => setQuery(e.target.value)}
               placeholder="Enter Class Code"
-              style={{ borderRadius: '15px', padding: '8px', fontSize: '2vw', textAlign: 'center', width: '20vw'}}
+              style={{ borderRadius: '15px', padding: '8px', fontSize: '2vw', textAlign: 'center', width: '20vw', height: '10vh'}}
             />
           </form>
         </div>
